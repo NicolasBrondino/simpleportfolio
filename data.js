80f2e476522ef90184330a01a3903708a0ced420
Nicolas Brondino <105771500+NicolasBrondino@users.noreply.github.com>
2023-02-07 15:51:30 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,155 @@
+/* Our tiny data structure in javascript :
+holds data about my dev projects */
+const healthcrops = {
+    priority: 0,
+    youtube:`<iframe width="470" height="246" style="border-radius: 0.8rem;" 
+    src="https://www.youtube.com/embed/-ZOuGysLdKY" title="YouTube video player" 
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+     gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    title: "Identifying crops' health condition in a video",
+    language: "C++",
+    domain: "computer vision",
+    library: "openCV",
+    description:`</br>Some agricultural sector IT companies developed the use of flying drones combined with vision capabilities to detect issues with certain types of crops.</br>
+     This OpenCV and C++ module simulates the detection of diseased maize crops using color and contrast.</br>
+     `,
+    havesource:"false",
+    source:"<i>context : Last year's academy projects. </br>Code from this era hasn't been saved.</i>",
+}
+const neurallearn = {
+    priority:0,
+    youtube:`<iframe width="470" height="246" style="border-radius: 0.8rem;" 
+    src="https://www.youtube.com/embed/Sc04Q3yuI-M" title="YouTube video player" 
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+     gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    title: "Teaching a neural network about symptoms and diseases",
+    language: "C++",
+    domain: "AI",
+    library: "STL",
+    description:`</br>A simple 2 layer neural network to be used as a mockup learning model for detection of human diseases based on symptoms. </br></br>
+    Input data is presented to the model, who then ''learn'' to detect medical conditions based on input symptoms.</br>
+    `,
+    havesource:"false",
+    source:"<i>context : Last year's academy projects. </br>Code from this era hasn't been saved.</i>",
+}
+const skin = {
+    priority:0,
+    youtube:`<iframe width="470" height="246" style="border-radius: 0.8rem;" 
+    src="https://www.youtube.com/embed/OBLTvlCcn1M" title="YouTube video player" 
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+     gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    title: "Identifying patterns on human skin",
+    language: "C++",
+    domain: "computer vision",
+    library: "openCV",
+    description:`</br>Using shape and RGB color detection, this OpenCV piece of code experiments with the concept of machine-identifying patterns on the human skin, specifically to classify moles from possible skin melanomas.</br>
+    `,
+    havesource:"false",
+    source:"<i>context : Last year's academy projects. </br>Code from this era hasn't been saved.</i>",
+}
+const robotcleaner = {
+    priority:0,
+    youtube:`<iframe width="470" height="246" style="border-radius: 0.8rem;" 
+    src="https://www.youtube.com/embed/-qTQzP36260" title="YouTube video player" 
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+     gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    title: "Simulation of a robot cleaner",
+    language: "C++",
+    domain: "robots",
+    library: "STL / SDL",
+    description: `</br>This C++ module has been written for the purpose of simulating the process of learning the surroundings of a room apartment for a Roomba-like robot cleaner.</br></br>
+    `,
+    havesource:"false",
+    source:"<i>context : Last year's academy projects. </br>Code from this era hasn't been saved.</i>",
+}
+const portfolio = {
+    priority:0,
+    youtube:`<iframe width="470" height="246" style="border-radius: 0.8rem;" 
+     src="https://www.youtube.com/embed/BQPWpM0X8pk?start=5" title="YouTube video player" 
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;
+     web-share" allowfullscreen></iframe>`,
+    //youtube:"https://www.youtube.com/embed/BQPWpM0X8pk",
+    title: "My portfolio website",
+    language: "HTML/CSS/JS",
+    domain: "Web",
+    library: "",
+    description:`</br>The website you are currently browsing. Static is written is Html and CSS, and dynamic part is in javascript. <br><br>
+    This isn't meant to be fancy, but fast, scalable, and reliable. <br><br>
+    I have provided the source below.</br><i></i>`,
+    havesource:"true",
+    source:"Github link",
+}
+
+
+
+// future projects
+const cppwork = {
+    priority:0,
+    youtube:`<iframe width="470" height="246" style="border-radius: 0.8rem;"
+    src="https://www.youtube.com/embed/TDxYjfzgvpg?start=3" title="YouTube video player" 
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+    gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+    title: "Cpp.Work",
+    language: "HTML/CSS/JS",
+    domain: "Web",
+    library: "",
+    /*description:`A job board for job remote C++ posts, worldwide audience. 
+    <br> avantage : projet web supplémentaire pour les recherches job web
+    <br> avantage : assez prestigieux pour une section individuelle sur le cv
+    <br> avantage : petit revenu possible, test microservices
+    <br> avantage : accès prioritaire à certains emplois
+    `,*/
+    description:`<br>A job board for job remote C and C++ posts, worldwide audience. <br><br>
+    C and C++ both together are represented in around 11% of the projects currently being
+    developed in the world today. <br><br> This makes these programming languages somewhat niche, and it's more 
+    difficult for programmers to find their next company that it is for say, more common languages specialists. <br><br>
+    This is why I wrote this board : to help C & C++ programmers find their next dream job more easily.<br><br>
+    Not supported by advertisement, and posting jobs is free for companies.<br><br>
+    It can be found at <a href="https://www.cpp.work/">Cpp.Work</a>.
+    `,
+    havesource:"false",
+    source:`I have released only some parts of the code, as it's a commercial project.<br>
+    Some (front end) javascript and (back end) PHP modules are available on github.<br>`,
+    //The source code isn't available for this project`,
+}
+
+
+
+const reminderCpp = {
+    priority:0,
+    youtube:`<iframe width="470" height="246" style="border-radius: 0.8rem;"
+    src="https://www.youtube.com/embed/luCADlQjKNs?start=9" title="YouTube video player" 
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+    gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+    title: "C++ Starter code for beginner and junior developpers",
+    language: "C++",
+    domain: "Learning ressource",
+    library: "STL",
+    description:`</br>
+    This collection contains various examples of key concepts for the C++ language. 
+     All essential concepts for the C++ 11 / 14 programmer in code form with clear commenting:</br></br>
+
+    - basics : vars and loops, exceptions, bitwise recursivity + including tail recursivity.</br>
+    - data structures : vector, set, unordered map, list. </br>
+    - memory : raw pointers, smart pointers, unique/shared/weak pointers + how is memory freed in each case.</br>
+    - OOP concepts : classes,structs, & inheritance. conditional operator, virtual, operator overload.</br>
+    - Cpp 11/14 concepts : iterators, auto, templates, move semantics, lambdas & range, size_t.</br>
+    - Cpp 11/14 multithreading : join, detach, mutexes, locks.</br>
+
+    Code available on github below.</br>
+    `,
+    havesource:"true",
+    source:`Github Link<br>
+    Click here.<br>`,
+}
+
+let projects = [];
+// The push order and print order on the website are the same, I should place most recent projects first.
+projects.push(reminderCpp);
+//projects.push(cppwork); à finir
+projects.push(portfolio);
+projects.push(healthcrops);
+projects.push(neurallearn);
+projects.push(skin);
+projects.push(robotcleaner);
+
